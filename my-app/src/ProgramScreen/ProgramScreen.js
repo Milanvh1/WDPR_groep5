9e0eb006f6cd4ffd29a13ee7d9b5259bcb2d9a0c
Robin <21104964@student.hhs.nl>
2023-01-19 10:58:57 +0100

    2e poging

--- my-app/src/ProgramScreen/ProgramScreen.js
+++ my-app/src/ProgramScreen/ProgramScreen.js
@@ -4,9 +4,10 @@ function ProgramScreen(){
 
     return(
 
-        <div class="e101_335"><span  class="e37_339">Rapunzel</span>
+        <div class="e101_335">
+            <span  class="e37_339">Rapunzel</span>
             <div class="e37_329"></div>
-        </div>
+
         <div class="e49_405">
             <div class="e37_330"></div><span  class="e37_331">Doornroosje</span>
             <div class="e101_336"><span  class="e37_334">5 dec - 10 dec</span>
